6dc73c48f5e291b7bf337122592d349e7658ec8c
energypremiumhub <energypremiumhub@gmail.com>
2026-07-01 17:49:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Prem-Energy Website
 
+console.log("Prem-Energy Website Loaded");
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
